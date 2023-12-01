5f18589c9ad636327c3c403920710095ec0503b8
asifmMustafa <asifmustafa.2o5@gmail.com>
2023-12-02 00:37:05 +0600

    updated setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ __gitsha__ = '{}'
 version_info = ({})
 """
     sha = get_hash()
-    with open('/app/GFPGAN/', 'r') as f:
+    with open('/app/GFPGAN/VERSION', 'r') as f:
         SHORT_VERSION = f.read().strip()
     VERSION_INFO = ', '.join([x if x.isdigit() else f'"{x}"' for x in SHORT_VERSION.split('.')])
 
